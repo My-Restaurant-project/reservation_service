0c03b3cfb785fd4a5ca1c7e135a4022ccf8b1746
Javohir <abdusamatovjavohir@gmail.com>
2024-07-10 17:09:35 +0500

    no changes

--- cmd/main.go
+++ cmd/main.go
@@ -10,34 +10,44 @@ import (
 	"reservation_service/services"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection" // Import the reflection package
 )
 
 func main() {
+	// Load configuration
 	config := c.Load()
 
+	// Connect to the database
 	db, err := pkg.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
+	// Create a TCP listener on specified port
 	listener, err := net.Listen("tcp", ":"+config.URL_PORT)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer listener.Close()
 
-	log.Println("Server started on port  " + config.URL_PORT)
+	log.Println("Server started on port " + config.URL_PORT)
 
+	// Initialize repository and services
 	reserve := repository.NewReservationRepo(db)
-
 	rs := services.NewReservationService(reserve)
 
+	// Create a new gRPC server
 	s := grpc.NewServer()
+
+	// Register the ReservationServiceServer with the gRPC server
 	pb.RegisterReservationServiceServer(s, rs)
 
+	// Register reflection service on gRPC server
+	reflection.Register(s)
+
+	// Start the gRPC server
 	if err := s.Serve(listener); err != nil {
 		log.Fatal(err)
 	}
-
 }
